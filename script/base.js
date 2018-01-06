bb6289bca12c5351a07dde2616701b7fcd6b6d0c
Myles <myles.developer@gmail.com>
2018-01-06 18:10:43 -0500

    Add collapsible behavior to blockquote

--- script/base.js
+++ script/base.js
@@ -1,3 +1,16 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    init();
 });
+
+function init() {
+    //Set class attribute to blockquotes
+    $("blockquote").addClass("collapse");
+
+    //Add folding button & behaviors after every blockquote element
+    $("blockquote").after("<a class='reactPriorQuote'>show/hide quotes</a>");
+    $(".reactPriorQuote").click(function(){
+        $(this).prev().collapse("toggle");
+    });
+
+
+}
