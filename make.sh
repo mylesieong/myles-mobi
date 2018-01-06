712444cc30a8f0965543a4acb06947ce5dc61f53
Myles <myles.developer@gmail.com>
2018-01-06 15:24:44 -0500

    Update bash after markdown.pl program change

--- make.sh
+++ make.sh
@@ -7,13 +7,7 @@ rm -f index.html
 cat static/header > index.html
 
 ## Print content
-#for f in content/*.md
-for f in $(find content/*.md -type f | sort -r)
-do
-    cat static/subheader >> index.html
-    perl bin/markdown.pl --html4tags $f >> index.html
-    cat static/subfooter >> index.html
-done
+perl bin/markdown.pl --html4tags content.md >> index.html
 
 ## Print footer 
 cat static/footer >> index.html
