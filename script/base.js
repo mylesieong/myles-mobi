f8cb606f8845fb3cfdd535792534bc7f6dd6c550
Myles <myles.developer@gmail.com>
2018-01-07 00:47:16 -0500

    Add attributes to elements

--- script/base.js
+++ script/base.js
@@ -3,14 +3,22 @@ $( document ).ready(function() {
 });
 
 function init() {
-    //Set class attribute to blockquotes
-    $("blockquote").addClass("collapse");
-
     //Add folding button & behaviors after every blockquote element
-    $("blockquote").after("<a class='reactPriorQuote'>show/hide quotes</a>");
+    $("blockquote").after("<a class='reactPriorQuote'>show/hide</a>");
     $(".reactPriorQuote").click(function(){
         $(this).prev().collapse("toggle");
     });
 
-
+    //Set attributes
+    $("section").addClass("font-yahei");
+    $("h1").addClass("font-medium");
+    $("div.container").children("p").addClass("font-small");
+    $("blockquote").addClass("collapse");
+    $("blockquote").find("p").addClass("font-xs")
+                                .addClass("font-italic")
+                                .addClass("font-lobster")
+                                .addClass("align-center")
+                                .addClass("fix-width-80")
+                                .addClass("margin-lr-auto")
+                                .addClass("color-ct-grey");
 }
